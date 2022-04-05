09a21bef2398ed007bd19bad42e8b712180b8e04
davebasler <davebasler@hotmail.com>
2022-04-05 22:34:31 +0200

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,40 +4,44 @@ import Home from "./pages/Home";
 import Music from './pages/Music';
 import Account from './pages/Account';
 import Album from './pages/Album';
+import Player from './helpers/Player';
 import './App.css';
 import {Layout} from 'antd';
 import Logo from "./images/Logo.png"; 
 const {Footer, Sider, Content} = Layout;
- 
 
 
 const App = () => {
   return(
     <Layout>
-      <Sider width={250} className="sideBar">
-        <img src={Logo} alt="Logo" classname="logo" style={{marginBottom:"50px"}}></img>
-        <Link to ="/">
-          <p style={{color: "#1986e6"}}> Home</p>
-        </Link>
-        <Link to ="/music">
-          <p style={{color: "#ffffff"}}> Explore</p>
-        </Link>
-        <Link to ="/account">
-          <p style={{color: "#ffffff"}}> My Account</p>
-        </Link>
-        
-      </Sider>
-      <Content>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/music" element={<Music />} />
-          <Route path="/album" element={<Album />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </Content>
+      <Layout>
+        <Sider width={250} className="sideBar">
+          <img src={Logo} alt="Logo" classname="logo" style={{marginBottom:"50px"}}></img>
+          <Link to ="/">
+            <p style={{color: "#ffffff"}}> Home</p>
+          </Link>
+          <Link to ="/music">
+            <p style={{color: "#ffffff"}}> Explore</p>
+          </Link>
+          <Link to ="/account">
+            <p style={{color: "#ffffff"}}> My Account</p>
+          </Link>
+          
+        </Sider>
+        <Content>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/album" element={<Album />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </Content>
 
+      </Layout>
+      <Footer className='footer'>
+        <Player />
+      </Footer>
     </Layout>
-   
   )
 };
 
