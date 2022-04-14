6b6f6886096867960db10b3eb2677062dcb82a85
Jason Browne <browne.jason@htomail.com>
2022-04-14 09:37:51 +0200

    continue with songs - listing songs per album

--- client/src/pages/Album.js
+++ client/src/pages/Album.js
@@ -3,6 +3,8 @@ import "./Album.css";
 import { useLocation } from 'react-router-dom';
 import {ClockCircleOutlined} from '@ant-design/icons';
 
+
+/*
 const albumDetails = [ //to be changed
   {
     src: "https://ipfs.moralis.io:2053/ipfs/Qmf8xEYZdMtQXYv56VxxmzbtUtEVjmaFaXGCgcBqGXDAA6/music/JTwinkle.mp3",
@@ -53,10 +55,82 @@ const albumDetails = [ //to be changed
     duration: "2:16",
   },
 ];
-
+*/
 const Album = () => {
 
   const {state: album} = useLocation();
+
+  const albumDetails = [
+
+    {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "4",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/3.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Head Shoulder\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/3\",\"duration\":\"0:09\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.133Z"
+  },
+  {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "6",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/5.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Pizza with a Coke\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/5\",\"duration\":\"0:09\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.133Z"
+  },
+  {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "5",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/4.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Old MC's Farm\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/4\",\"duration\":\"0:08\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.133Z"
+  },
+  {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "3",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/2.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Unbreak My Heart\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/2\",\"duration\":\"0:12\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.133Z"
+  },
+  {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "2",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/1.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Dogs Out\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/1\",\"duration\":\"0:09\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.133Z"
+  },
+  {
+      "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+      "token_id": "1",
+      "amount": "1",
+      "contract_type": "ERC721",
+      "name": "Shadow",
+      "symbol": "shad",
+      "token_uri": "https://gateway.moralisipfs.com/ipfs/QmcfAiN4gVRFDB3uqQKAN1hgpFk3bDG3hVVV2bBnDZNYsD/metadata/0.json",
+      "metadata": "{\"image\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/6\",\"name\":\"Always Love You\",\"animation_url\":\"ipfs://QmX5NMV8hh1g5EcebX1e2Y55uQnVnKPk8YzW37wpnRWfXp/media/0\",\"duration\":\"0:09\",\"artist\":\"Snoop Jay\",\"year\":\"2022\"}",
+      "synced_at": "2022-03-04T09:06:55.134Z"
+  }
+]
+
+
 
   //add smart contract call here later...
   const response = [];
@@ -86,8 +160,8 @@ const Album = () => {
       <div className='numberHeader'><ClockCircleOutlined/></div>
     </div>
     
-  </div>
-{ /*
+  
+ 
         {albumDetails &&
           albumDetails.map((song, i) => {
             song = JSON.parse(song.metadata);
@@ -105,8 +179,8 @@ const Album = () => {
                 </div>
               </>
             );
-          })} */}
-      
+          })} 
+      </div>
     </>
   );
 };
